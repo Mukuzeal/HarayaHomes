ac7c40f3589accba7c13b2e02f77f473cde2ce93
Mukuzeal <jmbandao30@gmail.com>
2025-09-29 01:29:54 +0800

    Initial Commit

---
+++ mobile.js
@@ -0,0 +1,22 @@
+const doc = document;
+const menuOpen = doc.querySelector(".menu");
+const menuClose = doc.querySelector(".close");
+const overlay = doc.querySelector(".overlay");
+
+menuOpen.addEventListener("click", () => {
+  overlay.classList.add("overlay--active");
+});
+
+menuClose.addEventListener("click", () => {
+  overlay.classList.remove("overlay--active");
+});
+  const haraya = document.getElementById("Haraya");
+  setInterval(() => {
+    haraya.classList.toggle("switch");
+  }, 5000); // every 5s
+  
+
+var counter = 200;
+var opacityCount = 0;
+var lastScroll = 0;
+
