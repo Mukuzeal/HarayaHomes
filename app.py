3e652b22547a89c42dd2da70b62dbd2581598c38
Mukuzeal <jmbandao30@gmail.com>
2025-09-30 20:00:47 +0800

    Did some coding off screen

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    return render_template("index.html")
+
+@app.route('/login')
+def login():
+    return render_template('LoginSignup/login.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- static/mobile.js
+++ static/mobile.js
@@ -4,19 +4,18 @@ const menuClose = doc.querySelector(".close");
 const overlay = doc.querySelector(".overlay");
 
 menuOpen.addEventListener("click", () => {
-  overlay.classList.add("overlay--active");
+    overlay.classList.add("overlay--active");
 });
 
 menuClose.addEventListener("click", () => {
-  overlay.classList.remove("overlay--active");
+    overlay.classList.remove("overlay--active");
 });
-  const haraya = document.getElementById("Haraya");
-  setInterval(() => {
+const haraya = document.getElementById("Haraya");
+setInterval(() => {
     haraya.classList.toggle("switch");
-  }, 5000); // every 5s
-  
+}, 5000); // every 5s
+
 
 var counter = 200;
 var opacityCount = 0;
 var lastScroll = 0;
-
